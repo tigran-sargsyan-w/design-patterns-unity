cc386aeac6f62c123ee3923630fd2f8ab9439001
Tigran Sargsyan <tigran.sargsyan.w@gmail.com>
2023-05-15 18:27:14 +0400

    - feat : singleton pattern

---
+++ Assets/Creational Patterns/Singleton Pattern/SingletonTest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace SingletonPattern
+{
+    public class SingletonTest : MonoBehaviour
+    {
+        private void Start()
+        {
+           var test = OtherManager.Instance;
+           var test2 = OtherManager.Instance;
+           
+           if (test == test2)
+           {
+               Debug.Log("Objects are the same instance");
+           }
+        }
+    }
+}
---
+++ Assets/Creational Patterns/Singleton Pattern/Structure/OtherManager.cs
@@ -0,0 +1,27 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+
+namespace SingletonPattern
+{
+    public class OtherManager 
+    {
+        private static OtherManager instance;
+        public static OtherManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new OtherManager();
+                }
+                return instance;
+            }
+        }
+
+        static OtherManager()
+        {
+            Debug.Log($"<color=yellow>{nameof(OtherManager)}-Non-MonoBehavior-Singleton Initialized</color>");
+        }
+
+    }
+}
