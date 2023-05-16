785ac988fe3338a2a3b69031f23069dc54d9629f
Tigran Sargsyan <tigran.sargsyan.w@gmail.com>
2023-05-16 22:17:03 +0400

    - feat : facade pattern

---
+++ Assets/Structural Patterns/Facade Pattern/Structure/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace FacadePattern
+{
+    public class Enemy
+    {
+        public void LookingForPlayer()
+        {
+            Debug.Log("Enemy Looking For Player");
+        }
+        public void MoveToPlayer()
+        {
+            Debug.Log("Enemy Move To Player");
+        }
+        public void Hit()
+        {
+            Debug.Log("Enemy Hit");
+        }
+        
+        public void TryToDodge()
+        {
+            Debug.Log("Enemy Try To Dodge");
+        }
+        public void TryToDefend()
+        {
+            Debug.Log("Enemy Try To Defend");
+        }
+        public void GetDamage()
+        {
+            Debug.Log("Enemy Get Damage");
+        }
+    }
+}
---
+++ Assets/Structural Patterns/Facade Pattern/Structure/EnemyState.cs
@@ -0,0 +1,8 @@
+namespace FacadePattern
+{
+    public enum EnemyState
+    {
+        Attack,
+        Defend
+    }
+}
