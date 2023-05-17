9e66e35d0099dd5da978869cc4fb43cb599b5cb2
Tigran Sargsyan <tigran.sargsyan.w@gmail.com>
2023-05-17 19:16:49 +0400

    - feat : proxy pattern

---
+++ Assets/Structural Patterns/Proxy Pattern/Structure/ResourceProxy.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace ProxyPattern
+{
+    public class ResourceProxy : IResource
+    {
+        private Resource resource;
+        
+        public ResourceProxy(Resource resource = null)
+        {
+            if (resource != null)
+            {
+               this.resource = resource; 
+            }
+        }
+        
+        public void Operation()
+        {
+            if (resource != null)
+            {
+                resource.Operation();
+            }
+            else
+            {
+                Debug.Log("<color=red>Operation from Proxy</color>");
+            }
+        }
+    }
+}
