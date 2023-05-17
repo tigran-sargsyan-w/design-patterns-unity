961c504becb5a9ec9d3a02e6d48e2fe170138ea7
Tigran Sargsyan <tigran.sargsyan.w@gmail.com>
2023-05-17 23:37:14 +0400

    - feat : flyweight pattern

---
+++ Assets/Structural Patterns/Flyweight Pattern/Structure/BaseStats.cs
@@ -0,0 +1,15 @@
+﻿namespace FlyweightPattern
+{
+    public class BaseStats
+    {
+        public int MaxHealth => maxHealth;
+        public int Attack => attack;
+        public int Defense => defense;
+        public int Speed => speed;
+
+        private int maxHealth = 100;
+        private int attack = 10;
+        private int defense = 20;
+        private int speed = 5;
+    }
+}
---
+++ Assets/Structural Patterns/Flyweight Pattern/Structure/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace FlyweightPattern
+{
+    public class Enemy
+    {
+        private BaseStats stats;
+        private int currentHealth;
+
+        public Enemy()
+        {
+            stats = new BaseStats();
+            currentHealth = stats.MaxHealth;
+        }
+
+        public void ShowStats()
+        {
+            Debug.Log($"Enemy Stats: Attack: {stats.Attack}, Defense: {stats.Defense}, Speed: {stats.Speed}");   
+            Debug.Log($"Enemy Health: {currentHealth}, Max Health: {stats.MaxHealth}");
+        }
+        
+        public void TakeDamage(int damage)
+        {
+            Debug.Log($"<color=red>Enemy took {damage} damage</color>");
+            currentHealth -= damage;
+        }
+    }
+}
