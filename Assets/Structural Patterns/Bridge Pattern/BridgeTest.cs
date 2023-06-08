0e893a11a2a59b314cd6714518654ba3a48eefad
Tigran Sargsyan <tigran.sargsyan.w@gmail.com>
2023-06-08 23:15:24 +0400

    - feat : bridge pattern

---
+++ Assets/Structural Patterns/Bridge Pattern/BridgeTest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace BridgePattern
+{
+    public class BridgeTest : MonoBehaviour
+    {
+        private void Start()
+        {
+            Character warrior = new Warrior(new MeleeAttack());
+            warrior.Attack();
+            //warrior.UseAbility();
+            
+            Character mage = new Mage(new MagicAttack());
+            //mage.Attack();
+            mage.UseAbility();
+        }
+    }
+}
---
+++ Assets/Structural Patterns/Bridge Pattern/Structure/Warrior.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace BridgePattern
+{
+    public class Warrior : Character
+    {
+        public Warrior(IAbility ability) : base(ability)
+        {
+        }
+
+        public override void Attack()
+        {
+            Debug.Log("Warrior attack");
+        }
+
+        public override void UseAbility()
+        {
+            ability.PerformAbility();
+        }
+    }
+}
