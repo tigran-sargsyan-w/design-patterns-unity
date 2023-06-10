a6455ffd37144d0db1214cd8b51b68d098652379
Tigran Sargsyan <tigran.sargsyan.w@gmail.com>
2023-06-10 19:51:37 +0400

    - feat : mediator pattern

---
+++ Assets/Behavioral Patterns/Mediator Pattern/MediatorTest.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace MediatorPattern
+{
+    public class MediatorTest : MonoBehaviour
+    {
+        private NPCMediator mediator;
+
+        private void Start()
+        {
+            mediator = new NPCMediator();
+            NPC villager1 = new Villager("Villager 1");
+            NPC villager2 = new Villager("Villager 2");
+            NPC soldier1 = new Soldier("Soldier 1");
+            NPC soldier2 = new Soldier("Soldier 2");
+
+            mediator.AddNPC(villager1);
+            mediator.AddNPC(villager2);
+            mediator.AddNPC(soldier1);
+            mediator.AddNPC(soldier2);
+
+            villager1.SetMediator(mediator);
+            villager2.SetMediator(mediator);
+            soldier1.SetMediator(mediator);
+            soldier2.SetMediator(mediator);
+
+            villager1.SendMessage("Hi there.I'm villager 1");
+            //villager2.SendMessage("Hi there.I'm villager 2");
+            //soldier1.SendMessage("Hi there.I'm soldier 1");
+            //soldier2.SendMessage("Hi there.I'm soldier 2");
+        }
+    }
+}
---
+++ Assets/Behavioral Patterns/Mediator Pattern/Structure/Soldier.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace MediatorPattern
+{
+    public class Soldier : NPC
+    {
+        private string name;
+        public Soldier(string name)
+        {
+            this.name = name;
+        }
+        
+        public override void SendMessage(string message)
+        {
+            Debug.Log("Soldier say : " + message);
+            mediator.SendMessage(message, this);
+        }
+
+        public override void ReceiveMessage(string message)
+        {
+            Debug.Log($"{name} listen about : " + message);
+        }
+    }
+}
