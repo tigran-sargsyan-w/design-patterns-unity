05c6c6711925c2253f76d737af5f9647b80967e6
Tigran Sargsyan <tigran.sargsyan.w@gmail.com>
2023-06-13 15:44:26 +0400

    - feat : visitor pattern

---
+++ Assets/Behavioral Patterns/Visitor Pattern/Structure/EffectApplier.cs
@@ -0,0 +1,8 @@
+﻿namespace VisitorPattern
+{
+    public abstract class EffectApplier
+    {
+        public abstract void ApplyToKnight(Knight knight);
+        public abstract void ApplyToWizard(Wizard wizard);
+    }
+}
---
+++ Assets/Behavioral Patterns/Visitor Pattern/VisitorTest.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace VisitorPattern
+{
+    public class VisitorTest : MonoBehaviour
+    {
+        private void Start()
+        {
+            var knight = new Knight();
+            var wizard = new Wizard();
+
+            var damageEffect = new DamageEffect();
+            var healEffect = new HealEffect();
+
+            wizard.ApplyEffect(damageEffect);
+            knight.ApplyEffect(damageEffect);
+            wizard.ApplyEffect(healEffect);
+            
+        }
+    }
+}
